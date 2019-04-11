a736294ffb504143f00de5774ec68bb4e531eb3d
Thomas Tran <tran.thomas66@gmail.com>
2019-04-11 15:15:01 +0000

    -A

--- test/parallel_tests.rb
+++ test/parallel_tests.rb
@@ -5,8 +5,8 @@ require 'parallel'
 require 'open3'
 
 TestOutputFolder = File.join(File.dirname(__FILE__), 'local_test_output')
-ProcessorsUsed = (Parallel.processor_count * 1 / 2).floor
-
+#ProcessorsUsed = (Parallel.processor_count * 1 / 2).floor
+ProcessorsUsed = 35
 class String
   # colorization
   def colorize(color_code)
